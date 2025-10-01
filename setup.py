ef8fdc1eabf86fa43b2034899a60df92b57b41d3
subashmathanraj <subashmathan1@gmail.com>
2025-10-01 16:29:18 +0530

    setup commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='subash',
+author_email='subashmathan1@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')  
+)
