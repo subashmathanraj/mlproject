53abfa85d97d85e7900fc2c4e376c938d0a4c8d8
subashmathanraj <subashmathan1@gmail.com>
2025-10-08 17:27:39 +0530

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
    _,_,exc_tb=error_detail.exc_info() 
@@ -18,10 +18,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)  # Initialize logging
-    try:
-        a=1/10
-    except Exception as e:
-        logging.info("Divide by zero")
-        print("Handled:", CustomException)
